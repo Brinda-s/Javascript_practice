7cac0e2a01b3892ed124ea3b9fc42ac8818aee40
brinda-s <brinda.anitha@gmail.com>
2025-01-03 17:12:19 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log("Hello from the external script");
+let name ="John";
+let surname ="Doe";
+console.log(name);
+console.log(surname);
+
+let age = 11;
+console.log(age);
+age=54;
+console.log(age);
+
+//to keep it constant
+const pi = 3.14;
+console.log(pi);
+console.log((3 + 2) - 76 * (1 + 1));
+console.log(23 + 97);
+console.log(1+2+3+3+5+6);
+console.log((4 + 6 + 9) / 77);
+
+
+let a = 10;
+console.log(a);
+console.log(9*a);
+
+let b = 9*a;
+
+console.log(b);
+
+const max = 57;
+let actual = max-13;
+let percentage = actual/max;
